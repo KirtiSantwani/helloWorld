d7f16f02d2999c012dbfefc3fb2efb1866756bbf
Kirti Santwani <kirtisantwani77@gmail.com>
2019-03-21 22:01:00 +0530

    Add files via upload

---
+++ fileHandling.py
@@ -0,0 +1,12 @@
+'''
+Write a program that prompts for a file name, then opens that file and 
+reads through the file,and print the contents of the file in upper case.
+
+'''
+
+fname = input("Enter file name: ")
+fh = open(fname)
+for i in fh:
+    y= i.upper()
+    z = y.rstrip()
+    print(z)
